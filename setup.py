81eac4cef465bd97d3f8f25d65c1fd50887fd215
Danijar Hafner <mail@danijar.com>
2016-03-26 14:34:08 +0100

    Add core package to setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ import setuptools
 if __name__ == '__main__':
     setuptools.setup(
         name='sets',
-        version='0.1.0',
+        version='0.1.1',
         description='Read datasets in a standard way.',
         url='http://github.com/danijar/sets',
         author='Danijar Hafner',
         author_email='mail@danijar.com',
         license='MIT',
-        packages=['sets'],
+        packages=['sets', 'sets.core'],
         install_requires=['numpy'],
     )
